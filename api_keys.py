a6df6687f9e1e34e905ceaf186232eb5554b27bf
GEORGE GARCIA <95520150+ggarcia949@users.noreply.github.com>
2023-01-16 23:23:52 -0800

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+
+# From the "project set-up" experience
+api_key = 'HPuQlbVYXZJZXyKnagdCFMIBW'
+api_key_secret = 'tOP7S4jjZ4VFQgflXEBFmS9Y6hoxJ2K7UK8EgtdQnQ6omBAnkD'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAFUQlQEAAAAAtoomXNYnnNybm3o0bNa0cTRAfFo%3DpyBf8LrjH2gasfOpcSGL8h7nDStbwjAFQL70hrnjvrFSsUQ0Bj'
+
